a1c040c1b4cc368ad519e6e3139e9f2c57b766d1
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-06-27 00:27:04 +0530

    added more in main home landing page the steps

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center my-20 text-center">
       <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
@@ -15,7 +17,7 @@ const Header = () => {
       <p className="text-center max-w-xl mx-auto mt-5">Un1eash your creativity with AI. Turn your imagination into visual art
         in seconds - just type, and watch the magic happen.</p>
 
-        <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">
+        <button onClick={()=>navigate("/result")} className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full hover:scale-105 transition-all duration-400">
             Generate Images
             <img className="h-6" src={assets.star_group}/>
         </button>
